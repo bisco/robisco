0dbe98faf7ed8ec46bfdf2392b7418e99fc91036
bisco <nobu+github@glleu.net>
2016-02-29 23:10:46 +0900

    support hubot-script/redmine.coffee

--- start.sh
+++ start.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
+function usage() {
+    echo "usage: ./start.sh (slack|debug)" 
+    echo "slack -> kick hubot with slack adapter"
+    echo "debug -> kick hubot without adapter"
+}
+
+
+if [ $# -ne 1 ]; then
+    usage;
+    exit
+fi
+
+
 export HUBOT_SLACK_TOKEN=$(cat slack_token)
 export HUBOT_SLACK_TEAM=nbisco
 export HUBOT_SLACK_BOTNAME=robisco
 
-./bin/hubot -a slack
+# Redmine Configurations
+if [ -e "redmine_ssl_url" ]; then
+    export HUBOT_REDMINE_SSL=$(cat redmine_ssl_url)
+fi
+export HUBOT_REDMINE_BASE_URL=$(cat redmine_url)
+export HUBOT_REDMINE_TOKEN=$(cat redmine_token)
+export HUBOT_REDMINE_IGNORED_USERS=""
+
+if [ $1 = "slack" ]; then
+    ./bin/hubot -a slack
+else
+    ./bin/hubot
+fi
